5a9cf311398f9e6b5c4bf0df09ec7403ff043c66
Dmitrii Shvets <dshvets@sfedu.ru>
2023-09-08 08:31:26 +0300

    Выполнено 1 задание 1 лабы и проведено сравнение двух реализаций: на деке и обход в ширину на очереди.

--- laba1/laba1.cpp
+++ laba1/laba1.cpp
@@ -4,92 +4,95 @@
 #include "laba1.h"
 
 #include <vector>
-#include <deque>
+#include <functional>
 #include <chrono>
+
+#include <queue>
+#include <unordered_set>
 class Node {
 public:
 	Node* parent;
 	int value;
-	char op;
 
-	Node(Node* _parent, int _value, int _op) :parent(_parent), value(_value), op(_op) {
-
-	}
+	Node(Node* _parent, int _value) :parent(_parent), value(_value) {}
 };
 
-Node* add(int index, std::deque<Node*>& dec) {
-	if (dec[index]->value == 100)return dec[index];
-	if (dec[index]->value > 100)return dec[index - 1];
-	if (dec[index]->value * 2 <= 100) {
-		auto tmp2 = new Node{ dec[index],dec[index]->value * 2 ,'*' };
-		dec.emplace_back(tmp2);
-	}
-	if (dec[index]->value + 3 <= 100) {
-		auto tmp1 = new Node{ dec[index],dec[index]->value + 3 ,'+' };
-		dec.emplace_back(tmp1);
-	}
-
-
-	add(index++, dec);
+inline int op1(int x) {
+	return x + 4;
 }
+inline int op2(int x) {
+	return x * 2;
+}
+
+Node* task1(int a, int b, std::vector<std::function<int(int)>> &lambdas) {
+	std::queue<Node*> q;
+	std::unordered_set<int> s;
+	auto start = new Node{ nullptr,a };
+	q.push(start);
+
+	while (!q.empty())
+	{
+		Node* cur = q.front();
+		q.pop();
+		s.insert(cur->value);
+
+		for (auto& fn : lambdas) {
+			int value = fn(cur->value);
+			if (value > b)continue;
+			if (!s.contains(value)) {
+				auto tmp = new Node{ cur,value };
+				q.push(tmp);
+				s.insert(value);
+			}
+			if (value == b)return q.front();
+		}
+	}
+
+	return nullptr;
+}
+
+
+
 int main()
 {
-	auto start_time = std::chrono::steady_clock::now();
-	Node* a = new Node{ nullptr,2,' ' };
-	std::deque<Node*>dec;
-	dec.push_back(a);
-	int index = 0;
-	Node* cur = dec[index];
-
-	while (cur->value != 100)
-	{
-		if (dec[index]->value * 2 <= 100) {
-			auto tmp2 = new Node{ dec[index],dec[index]->value * 2 ,'*' };
-			dec.emplace_back(tmp2);
-		}
-		if (dec[index]->value + 3 <= 100) {
-			auto tmp1 = new Node{ dec[index],dec[index]->value + 3 ,'+' };
-			dec.emplace_back(tmp1);
-		}
-
-		cur = dec[++index];
-
-	}
-
-
-
-
-	std::vector<char> operations;
 	int count = 0;
-	if (cur) {
+	std::vector<std::function<int(int)>> lambdas;
+	lambdas.push_back(op1);
+	lambdas.push_back(op2);
 
-		auto tmp = cur;
-		while (tmp)
-		{
-			//std::cout << tmp->value << " ";
-			//operations.push_back(tmp->op);
-			tmp = tmp->parent;
-			count++;
-		}
-		/*std::cout << std::endl;
-		std::reverse(operations.begin(), operations.end());
-		for (auto x : operations) {
-			std::cout << x << " ";
-		}*/
+	auto start_time = std::chrono::steady_clock::now();
+
+	Node* last = task1(2, 10000001, lambdas);
+
+	while (last)
+	{
+		count++;
+		last = last->parent;
 	}
-	else {
-		std::cout << "error" << std::endl;
-	}
+
 	auto end_time = std::chrono::steady_clock::now();
 	auto elapsed_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time);
-	std::cout << elapsed_ns.count() << " ns\n" << "count: " << count << std::endl;
 
-	//std::vector<>
+	std::cout << elapsed_ns.count() << " ns\n" << "count: " << count << std::endl;
 	
 	return 0;
 }
-
+//на деке было при 100
 // x64-Debug 153700 ns
 // x64-Release 20500 ns
 // x86-Debug 229300 ns
 // x86-Release 17700 ns
+
+
+//на очереди было при 100
+// x64-Debug    57800 ns
+// x64-Release   7900 ns
+// x86-Debug     9200 ns
+// x86-Release  17700 ns
+
+
+//на очереди было при 10000001
+// x64-Debug   9671635800 ns 
+// x64-Release 2590164900 ns
+// x86-Debug  14581367500 ns
+// x86-Release 1940751900 ns
