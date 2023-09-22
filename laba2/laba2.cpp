f2577054b95bc6f6a735f0805bb59f2d4afc49dd
Dmitrii Shvets <dshvets@sfedu.ru>
2023-09-22 11:02:27 +0300

    Испрваленная лаба 2. Теперь алгоритм IDA* работает правильно.

--- laba2/laba2.cpp
+++ laba2/laba2.cpp
@@ -11,6 +11,8 @@
 
 unsigned long long END = 1311768467463790320;
 
+
+
 int bfs(const std::string& start) {
 	auto start_t = std::chrono::high_resolution_clock::now();
 
@@ -205,10 +207,11 @@ int ids(const std::string& start, int depth_limit) {
 	{
 		auto cur = q.top();
 		q.pop();
+
 		if (cur->position == END) {
 			auto end_t = std::chrono::high_resolution_clock::now();
 			auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end_t - start_t);
-			std::cout << "время ids " << duration.count() << " ns " << duration.count() / 1e+9 << " s\n";
+			std::cout << "found depth_limit: " << depth_limit << "время ids " << duration.count() << " ns " << duration.count() / 1e+9 << " s\n";
 			save_positions(cur, "ids.txt");
 			for (auto& i : s)
 			{
@@ -216,9 +219,11 @@ int ids(const std::string& start, int depth_limit) {
 			}
 			return cur->depth;
 		}
+
 		auto previon_cur = s.find(cur->position);
 		if (previon_cur != s.end()) {
 			if ((*previon_cur).second->depth > cur->depth) {
+				delete (*previon_cur).second;
 				s.erase(previon_cur);
 			}
 			else continue;
@@ -261,7 +266,9 @@ int ids(const std::string& start, int depth_limit) {
 		}
 
 	}
-
+	auto end_t = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end_t - start_t);
+	std::cout << "not found depth_limit: " << depth_limit << " время IDA* " << duration.count() << " ns " << duration.count() / 1e+9 << " s\n";
 	for (auto& i : s)
 	{
 		delete i.second;
@@ -346,7 +353,7 @@ int a_star(const std::string& start) {
 
 int ida(const std::string& start, int bound) {
 	int INF = 1e9;
-	std::priority_queue<Position*, std::vector<Position*>, ManhattanComparePositions> q;
+	std::stack<Position*> q;
 	std::unordered_map<unsigned long long, Position*> s;
 	auto start_t = std::chrono::high_resolution_clock::now();
 
@@ -359,11 +366,11 @@ int ida(const std::string& start, int bound) {
 
 		auto f = cur->depth + cur->hdm();
 		if (f > bound) {
-			auto end_t = std::chrono::high_resolution_clock::now();
-			auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end_t - start_t);
-			std::cout << "not found bound: " << bound << " время IDA* " << duration.count() << " ns " << duration.count() / 1e+9 << " s\n";
-			return f;
+			if (INF > f)INF = f;
+			delete cur;
+			continue;
 		}
+
 
 		if (cur->position == END) {
 			auto end_t = std::chrono::high_resolution_clock::now();
@@ -382,39 +389,27 @@ int ida(const std::string& start, int bound) {
 		if (cur->last_move != -4) {
 			if (cur->zero >= 4) {
 				auto new_pos = ((cur->position & mask_a[cur->zero - 4]) >> 16) | (cur->position & ~mask_a[cur->zero - 4]);
-
-				if (!s.contains(new_pos)) {
-					q.emplace(new Position{ cur, new_pos, (uint8_t)(cur->zero - 4), 4, cur->depth + 1 });
-				}
+				q.emplace(new Position{ cur, new_pos, (uint8_t)(cur->zero - 4), 4, cur->depth + 1 });
 			}
 		}
 		if (cur->last_move != 4) {
 			if (cur->zero <= 11) {
 				auto new_pos = ((cur->position & mask_a[cur->zero + 4]) << 16) | (cur->position & ~mask_a[cur->zero + 4]);
-
-				if (!s.contains(new_pos)) {
-					q.emplace(new Position{ cur, new_pos, (uint8_t)(cur->zero + 4), -4, cur->depth + 1 });
-				}
+				q.emplace(new Position{ cur, new_pos, (uint8_t)(cur->zero + 4), -4, cur->depth + 1 });
 			}
 
 		}
 		if (cur->last_move != -1) {
 			if (cur->zero % 4 != 0) {
 				auto new_pos = ((cur->position & mask_a[cur->zero - 1]) >> 4) | (cur->position & ~mask_a[cur->zero - 1]);
-
-				if (!s.contains(new_pos)) {
-					q.emplace(new Position{ cur, new_pos, (uint8_t)(cur->zero - 1), 1, cur->depth + 1 });
-				}
+				q.emplace(new Position{ cur, new_pos, (uint8_t)(cur->zero - 1), 1, cur->depth + 1 });
 			}
 
 		}
 		if (cur->last_move != 1) {
 			if (cur->zero % 4 != 3) {
 				auto new_pos = ((cur->position & mask_a[cur->zero + 1]) << 4) | (cur->position & ~mask_a[cur->zero + 1]);
-
-				if (!s.contains(new_pos)) {
-					q.emplace(new Position{ cur, new_pos, (uint8_t)(cur->zero + 1), -1, cur->depth + 1 });
-				}
+				q.emplace(new Position{ cur, new_pos, (uint8_t)(cur->zero + 1), -1, cur->depth + 1 });
 			}
 
 		}
@@ -446,33 +441,45 @@ int main() {
 	std::string pos61{ "BAC0F478E19623D5" };
 
 
-	std::string start_pos = pos55;
+	std::string start_pos = pos22;
 
-	//auto res1 = bfs(start_pos);
-	//std::cout << "count: " << res1 << std::endl;
+	auto res1 = bfs(start_pos);
+	std::cout << "count: " << res1 << std::endl;
 
 	//auto res2 = dfs(start_pos);
 	//std::cout << "count: " << res2 << std::endl;
 
-	//int res3;
-	//for (int i = 0; i < 100; i++)
-	//{
-	//	res3 = ids(start_pos, i);
-	//	if (res3 > 0)break;
-	//}
-	//std::cout << "count: " << res3 << std::endl;
-
-	auto res4 = a_star(start_pos);
-	std::cout << "count: " << res4 << std::endl;
-
-	int bound = Position::heuristic(hex_to_int(start_pos));
-	while (true) {
-		bound = ida(start_pos, bound);
-		if (bound == 0)break;
+	int res3;
+	for (int i = 0; i < 100; i++)
+	{
+		res3 = ids(start_pos, i);
+		if (res3 > 0)break;
 	}
+	std::cout << "count: " << res3 << std::endl;
+
+	//auto res4 = a_star(start_pos);
+	//std::cout << "count: " << res4 << std::endl;
+
+	//int bound = Position::heuristic(hex_to_int(start_pos));
+	//while (true) {
+	//	bound = ida(start_pos, bound);
+	//	if (bound == 0)break;
+	//}
 	//
 	//std::cout << "count: " << bound << std::endl;
 
 	return 0;
 }
 
+//not found bound : 41 время IDA * 1600 ns 1.6e-06 s
+//not found bound : 43 время IDA * 12300 ns 1.23e-05 s
+//not found bound : 45 время IDA * 77400 ns 7.74e-05 s
+//not found bound : 47 время IDA * 725300 ns 0.0007253 s
+//not found bound : 49 время IDA * 4774300 ns 0.0047743 s
+//not found bound : 51 время IDA * 40825300 ns 0.0408253 s
+//not found bound : 53 время IDA * 247856200 ns 0.247856 s
+//not found bound : 55 время IDA * 1613966600 ns 1.61397 s
+//not found bound : 57 время IDA * 10721144100 ns 10.7211 s
+//not found bound : 59 время IDA * 79700620600 ns 79.7006 s
+//found 1 bound : 61 время IDA * 137269282300 ns 137.269 s
+//итого 230 сек
